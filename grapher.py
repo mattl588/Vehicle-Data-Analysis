f9d5a4b283a270b18804802ad1cb0af3cd55c7cf
Matt Lacaire <mattlacaire@gmail.com>
2022-11-14 21:35:19 -0800

    Added base files

---
+++ grapher.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 21:34:22 2022
+
+@author: Steve
+"""
+
---
+++ writer_and_reader.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 21:33:44 2022
+
+@author: Steve
+"""
+
