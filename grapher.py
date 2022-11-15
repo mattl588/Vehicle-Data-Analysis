1431b7a97056e9f94fa6639d5db3668c15c0efae
Matt Lacaire <mattlacaire@gmail.com>
2022-11-14 22:45:37 -0800

    Initial rudimentary JSON-writing script

--- grapher.py
+++ grapher.py
@@ -5,3 +5,4 @@ Created on Mon Nov 14 21:34:22 2022
 @author: Steve
 """
 
+#Inputs: year, country, condition, color, edition, RHD/LHD, 
--- writer_and_reader.py
+++ writer_and_reader.py
@@ -5,3 +5,47 @@ Created on Mon Nov 14 21:33:44 2022
 @author: Steve
 """
 
+#Inputs: year, country, condition, color, edition, RHD/LHD, 
+
+import json
+from os.path import exists
+
+year = input("What's the year?\n")
+
+country = input("What country is it from?\n")
+
+condition = input("Is the condition poor, fair, good, or excellent?\n")
+
+color = input("What is the color?\n")
+
+edition = input("Is this a 90 or 110?\n")
+
+date = input("Date of listing in MM/DD/YY format?\n")
+
+wheel = input("RHD or LHD?\n")
+
+file = "data.json"
+file_exists = exists(file)
+
+vehicle = {
+        "year": year,
+        "country": country,
+        "condition": condition,
+        "color": color,
+        "edition": edition,
+        "wheel": wheel
+}
+
+if (file_exists):
+    with open(file) as f:
+        unpacked_data = json.load(f)
+        if type(unpacked_data) is dict:
+            unpacked_data = [unpacked_data]
+        print(unpacked_data)
+        unpacked_data.append(vehicle)
+    with open(file, 'w') as json_file:
+        json.dump(unpacked_data, json_file, indent = 4)
+
+else: 
+    with open ("data.json", 'w') as j:
+        json.dump(vehicle, j, indent = 4)
